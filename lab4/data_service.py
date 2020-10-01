3120a1c9acf2f0fd8f77a001aa523aa201a37a87
Тимошенко Володимир <71499371+VovaTymoshenko1@users.noreply.github.com>
2020-10-01 12:31:59 +0300

    Update data_service.py

--- lab4/data_service.py
+++ lab4/data_service.py
@@ -1,1 +1,21 @@
-preint("data_service")
+from dataclasses import dataclass
+
+code = [110, 120, 130, 140, 150]
+name = ["ІНКО", "АЖІО", "Градобанк", "Відродження", "Укрінбанк"]
+
+@dataclass
+class dovidnik:
+   code:int
+   name:str
+
+
+data_array = [dovidnik(110, "ІНКО"), dovidnik(120,"АЖІО"), dovidnik(130,"Градобанк"), dovidnik(140, "Відродження"), dovidnik(150,"Укрінбанк")]
+
+def printdovidnik(dovidnik):
+    '''printdovidnik function prints with names and values'''
+    print("Код {code}, Назва {name}" .format(code = dovidnik.code, name = dovidnik.name))
+
+for data in data_array:
+    printdovidnik(data)
+
+    
